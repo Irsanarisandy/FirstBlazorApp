a79de145ba6989683c59a5f890e9e89ba92f02b8
Irsan Arisandy <irsanarisandy@hotmail.com>
2020-01-11 21:55:58 +1100

    Separate logic from view

---
+++ Logic/CounterLogic.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FirstBlazorApp.Logic
+{
+    public class CounterLogic : ComponentBase
+    {
+        protected int currentCount = 0;
+
+        [Parameter]
+        public int IncrementAmount { get; set; } = 1;
+
+        protected void IncrementCount()
+        {
+            currentCount += IncrementAmount;
+        }
+    }
+}
---
+++ Logic/TodoLogic.cs
@@ -0,0 +1,21 @@
+﻿using FirstBlazorApp.Data;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace FirstBlazorApp.Logic
+{
+    public class TodoLogic : ComponentBase
+    {
+        protected IList<TodoItem> todos = new List<TodoItem>();
+        protected string newTodo;
+
+        protected void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
